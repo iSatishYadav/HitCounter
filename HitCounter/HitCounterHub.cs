62f5bdcdcdb2321c75ab28ea705535176f97d271
Satish Yadav <monstersatish@live.com>
2015-08-15 08:16:23 +0530

    Init

---
+++ HitCounter/HitCounterHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HitCounter
+{
+    public class HitCounterHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        static int _hitCounter;
+
+        public void Hit()
+        {
+            _hitCounter++;
+            Clients.All.hit(_hitCounter);
+        }
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            _hitCounter--;
+            Clients.All.hit(_hitCounter);
+            return base.OnDisconnected(stopCalled);
+        }
+
+    }
+}
